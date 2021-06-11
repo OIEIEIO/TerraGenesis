566ae35749a6ca3c881e6fbaeca3094b8c03d57a
OIEIEIO <oieieio@protonmail.com>
2021-06-11 11:11:34 -0400

    Update cryptonote_config.h

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -201,7 +201,7 @@ namespace config
   boost::uuids::uuid const NETWORK_ID = { {
       0x12 ,0x11, 0xF1, 0x71 , 0x61, 0x04 , 0x41, 0x61, 0x17, 0x31, 0x00, 0x82, 0x16, 0xA1, 0xA1, 0x10
     } }; // Buckwheat's milkshake
-  std::string const GENESIS_TX = "013c01ff0001ffffffffff1f029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd08807121012a1a936be5d91c01ee876e38c13fab0ee11cbe86011a2bf7740fb5ebd39d267d";
+  std::string const GENESIS_TX = "";
   uint32_t const GENESIS_NONCE = 70;
 
   namespace testnet
