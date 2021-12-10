800db587c4b69e5f42177301fdc4bc234b624b59
OIEIEIO <oieieio@protonmail.com>
2021-12-09 21:50:54 -0500

    Update net_node.h

--- src/p2p/net_node.h
+++ src/p2p/net_node.h
@@ -281,7 +281,10 @@ namespace nodetool
 
   private:
     const std::vector<std::string> m_seed_nodes_list =
-    {  "31.187.74.144"
+    {   "10.63.93.141"
+        ,"10.63.93.128"
+        ,"10.63.93.70"
+        ,"10.63.93.45"
             
     };
 
